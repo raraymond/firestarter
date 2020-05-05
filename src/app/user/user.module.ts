9455c8221a9b9d144050d934f5c6bf60d7c90307
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-04 20:10:16 -0400

    finished sign in register and forgot password pages now need to add auth gaurds

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,9 +6,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { EmailLoginComponent } from './email-login/email-login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
-  imports: [CommonModule, UserRoutingModule, SharedModule],
+  declarations: [
+    LoginPageComponent,
+    GoogleSigninDirective,
+    EmailLoginComponent,
+  ],
+  imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class UserModule {}
